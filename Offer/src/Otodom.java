b641721a209af1ad7ac8b512effbfb5c8fccfe07
widuch.karol <karol_widuch@interia.pl>
2020-03-15 22:47:46 +0100

    Threads: offer downloader and saver - repair

--- Offer/src/Otodom.java
+++ Offer/src/Otodom.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Set;
 import java.util.TreeSet;
@@ -7,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Otodom {
-    public static String readWebsite(String link) throws IOException {
+    static String readWebsite(String link) throws IOException {
         URL url = new URL(link);
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(url.openStream()));
@@ -21,17 +20,16 @@ public class Otodom {
         return stringBuilder.toString();
     }
 
-    public static void saveWebsite(String content, String fileName) throws IOException {
+    static void saveWebsite(String content, String fileName) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, false));
         bufferedWriter.write(content);
         bufferedWriter.close();
     }
 
-    public static void main(String[] args) throws Exception {
+    static void download(String link, int threadsNumber) throws IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(threadsNumber);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(15);
-
-        String content = readWebsite("https://www.otodom.pl/sprzedaz/mieszkanie/krakow/");
+        String content = readWebsite(link);
         Set<String> offerLinks = new TreeSet<>();
 
         for (int i = 0; i < content.length(); ++i) {
@@ -39,20 +37,26 @@ public class Otodom {
             if(i < 0){
                 break;
             }
-            String link = content.substring(i).split(".html")[0];
-            offerLinks.add(link);
+            String subLink = content.substring(i).split(".html")[0];
+            offerLinks.add(subLink);
         }
         for (String offerLink : offerLinks) {
             executorService.submit(() -> {
                         try {
-                            saveWebsite(readWebsite(offerLink), offerLink.substring(offerLink.indexOf("oferta/")+7) + ".html");
+                            saveWebsite(readWebsite(offerLink),
+                                    offerLink.substring(offerLink.indexOf("oferta/")+7) + ".html");
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
             );
-
         }
         executorService.shutdown();
+    }
+
+    public static void main(String[] args) throws Exception {
+        download("https://www.otodom.pl/sprzedaz/mieszkanie/warszawa", 15);
+        download("https://www.otodom.pl/sprzedaz/mieszkanie/krakow", 10);
+        download("https://www.otodom.pl/sprzedaz/mieszkanie/gdansk", 30);
     }
 }
